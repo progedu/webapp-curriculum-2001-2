b144ab377ad1ebc649e377a7b7caee92d506fc5f
nonoike <12398740+nonoike@users.noreply.github.com>
2018-07-05 02:40:34 +0900

    挑戦中級 副作用のないメソッドに変更

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -2,8 +2,6 @@ object Main {
 
   class Switch(var isOn: Boolean)
 
-  def toggle(switch: Switch): Unit = {
-    if (switch.isOn) switch.isOn = false else switch.isOn = true
-  }
+  def toggle(switch: Switch): Switch = new Switch(!switch.isOn)
 
 }
