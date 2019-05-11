988926389c72c5e05d750228af8cee6afc15abeb
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2019-05-11 21:29:03 +0900

    副作用のないメソッドに変更

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,14 @@
-object Main {
+import scala.annotation.tailrec
 
-  class Switch(var isOn: Boolean)
+object Main {
+  //  2001-1
+  @tailrec
+  def fact(n: Int, acc: Int): Int = if (n <= 1) acc else fact(n - 1, acc * n)
+
+  //  2001-2
+  case class Switch(var isOn: Boolean)
 
   def toggle(switch: Switch): Unit = {
-    if (switch.isOn) switch.isOn = false else switch.isOn = true
+    if (switch.isOn) Switch(false) else Switch(true)
   }
-
 }
