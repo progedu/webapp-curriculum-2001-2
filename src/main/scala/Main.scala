6872d8166827d0d2c31eb87ce9cfa10f44b62273
Ryusuke Iida <iida-ryusuke469@g.ecc.u-tokyo.ac.jp>
2020-03-11 09:25:42 +0900

    副作用のないメソッドに変更

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,9 @@
 object Main {
 
-  class Switch(var isOn: Boolean)
+  case class Switch(var isOn: Boolean)
 
   def toggle(switch: Switch): Unit = {
-    if (switch.isOn) switch.isOn = false else switch.isOn = true
+    if (switch.isOn) Switch(false) else Switch(true)
   }
 
 }
